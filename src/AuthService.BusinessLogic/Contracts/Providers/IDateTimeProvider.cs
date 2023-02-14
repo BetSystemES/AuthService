a5bbffdf8935231805a70063b5050a800da2c094
bynikk <bynik2001@gmail.com>
2023-02-14 10:51:54 +0300

    Implement DateTime provider

---
+++ src/AuthService.BusinessLogic/Contracts/Providers/IDateTimeProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthService.BusinessLogic.Contracts.Providers
+{
+    /// <summary>
+    /// Data time provider contract.
+    /// </summary>
+    public interface IDateTimeProvider
+    {
+        DateTime Now { get; }
+
+        DateTime NowUtc { get; }
+    }
+}
