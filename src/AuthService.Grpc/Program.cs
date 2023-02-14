f568a404169068b92e63c6449be40a7451b3f9d2
bynikk <bynik2001@gmail.com>
2023-02-14 08:53:08 +0300

    hotfix build

--- src/AuthService.Grpc/Program.cs
+++ src/AuthService.Grpc/Program.cs
@@ -1,6 +1,4 @@
-using AuthService.Grpc.Services;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
@@ -11,7 +9,6 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
