d44018a82b9d4fa2a39ed62645009c0933753c2d
bynikk <bynik2001@gmail.com>
2023-02-14 08:52:46 +0300

    add database context

---
+++ src/AuthService.DataAccess/AuthDbContext.cs
@@ -0,0 +1,46 @@
+﻿using AuthService.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.DataAccess
+{
+    /// <summary>
+    /// Db context for Auth Database
+    /// </summary>
+    /// <seealso cref="Microsoft.EntityFrameworkCore.DbContext" />
+    public class AuthDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options) { }
+
+        /// <summary>
+        /// Override this method to further configure the model that was discovered by convention from the entity types
+        /// exposed in <see cref="T:Microsoft.EntityFrameworkCore.DbSet`1" /> properties on your derived context. The resulting model may be cached
+        /// and re-used for subsequent instances of your derived context.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context. Databases (and other extensions) typically
+        /// define extension methods on this object that allow you to configure aspects of the model that are specific
+        /// to a given database.</param>
+        /// <remarks>
+        /// <para>
+        /// If a model is explicitly set on the options for this context (via <see cref="M:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)" />)
+        /// then this method will not be run.
+        /// </para>
+        /// <para>
+        /// See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information.
+        /// </para>
+        /// </remarks>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRefreshTokenConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/AuthService.DataAccess/AuthServiceContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AuthService.DataAccess
+{
+    /// <summary>
+    /// Auction service context factory
+    /// </summary>
+    public class AuthServiceContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
+    {
+        /// <summary>
+        /// Creates a new instance of a derived context.
+        /// </summary>
+        /// <param name="args">Arguments provided by the design-time service.</param>
+        /// <returns>
+        /// An instance of <typeparamref name="TContext" />.
+        /// </returns>
+        public AuthDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AuthDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=AuthDb;User Id=postgres;Password=123");
+
+            return new AuthDbContext(optionsBuilder.Options);
+        }
+    }
+}
