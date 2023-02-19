313729ae7b1ea6ac3e72a9296bbfbfa73d69b11d
bynikk <bynik2001@gmail.com>
2023-02-20 01:11:50 +0300

    fix datatime provider

--- src/AuthService.BusinessLogic/Contracts/Providers/IDateTimeProvider.cs
+++ src/AuthService.BusinessLogic/Contracts/Providers/IDateTimeProvider.cs
@@ -5,8 +5,20 @@
     /// </summary>
     public interface IDateTimeProvider
     {
+        /// <summary>
+        /// Gets the now.
+        /// </summary>
+        /// <value>
+        /// The now DateTime.
+        /// </value>
         DateTime Now { get; }
 
+        /// <summary>
+        /// Gets the now UTC.
+        /// </summary>
+        /// <value>
+        /// The now UTC DateTime.
+        /// </value>
         DateTime NowUtc { get; }
     }
 }
