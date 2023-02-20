8b0bc527ab92f937e3df0a7f63d350b64f295209
bynikk <bynik2001@gmail.com>
2023-02-20 22:59:36 +0300

    implement migrator

---
+++ src/AuthService.DatabaseMigrator/Extensions/MigrationDatabaseExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AuthService.DatabaseMigrator.Extensions
+{
+    public static class MigrationDatabaseExtensions
+    {
+        public static IServiceProvider MigrateDatabaseFromContext<TContext>(this IServiceProvider serviceProvider) where TContext : DbContext
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                var context = serviceProvider.GetRequiredService<TContext>();
+
+                context.Database.Migrate();
+
+                logger.LogTrace("Migration successfully completed");
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical("During migration error occurred: {Message}", e.Message);
+
+                throw;
+            }
+
+            return serviceProvider;
+        }
+    }
+}
---
+++ src/AuthService.DatabaseMigrator/Program.cs
@@ -0,0 +1,60 @@
+﻿using AuthService.DataAccess;
+using AuthService.DatabaseMigrator.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.CommandLine;
+
+var rootCommand = new RootCommand("Migrate database by connection string via EntityFramework");
+var connectionStringSourceOption = new Option<string>("--connection-string-source",
+    "Connection String Source: Available options: env, option");
+var connectionStringOption = new Option<string>("--connection-string",
+    "Connection string for connect to database");
+var connectionStringEnvVariableName = new Option<string>("--connection-string-env-variable-name",
+    "Name of env variable which contains the connection string");
+
+rootCommand.AddOption(connectionStringSourceOption);
+rootCommand.AddOption(connectionStringOption);
+rootCommand.AddOption(connectionStringEnvVariableName);
+
+rootCommand.SetHandler<string, string, string>(MigrateDatabase,
+    connectionStringSourceOption,
+    connectionStringOption,
+    connectionStringEnvVariableName);
+
+return await rootCommand.InvokeAsync(args);
+
+static void MigrateDatabase(string source, string connection, string envName)
+{
+    if (source != "env" && source != "option")
+    {
+        throw new InvalidDataException("Invalid value for '--connection-string-source' option");
+    }
+
+    var connectionString = source == "option"
+        ? connection
+        : Environment.GetEnvironmentVariable(envName);
+
+    MigratePostgreSqlServer(connectionString!);
+}
+
+static void Migrate<TContext>(Func<IServiceCollection, IServiceCollection> configure)
+    where TContext : DbContext
+{
+    var services = new ServiceCollection()
+                        .AddLogging(configure => configure.AddConsole());
+
+    configure(services)
+        .BuildServiceProvider()
+        .MigrateDatabaseFromContext<TContext>()
+        .AddRolesIfNotPresented<TContext>();
+}
+
+static void MigratePostgreSqlServer(string connectionString)
+{
+    Migrate<AuthDbContext>(services =>
+        services.AddPostgresContext(options =>
+        {
+            options.UseNpgsql(connectionString);
+        }));
+}
