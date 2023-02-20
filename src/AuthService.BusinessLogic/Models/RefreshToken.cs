745bccb4bb89a0a4c673ed1c312e6ebac46d012d
bynikk <bynik2001@gmail.com>
2023-02-20 10:59:15 +0300

    add token models

---
+++ src/AuthService.BusinessLogic/Models/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.BusinessLogic.Models
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; }
+
+        public DateTime IssuedAtUtc { get; set; }
+
+        public DateTime ExpiresAtUtc { get; set; }
+    }
+}
