aef5384f56fbc243b60b42191783766573b75c5c
bynikk <bynik2001@gmail.com>
2023-02-20 10:58:38 +0300

    Implement jwt worker

---
+++ src/AuthService.BusinessLogic/Contracts/Worker/IJWTWorker.cs
@@ -0,0 +1,27 @@
+﻿using AuthService.BusinessLogic.Models;
+
+namespace AuthService.BusinessLogic.Contracts.Worker
+{
+    /// <summary>
+    /// Jwt worker contract.
+    /// </summary>
+    public interface IJWTWorker
+    {
+        /// <summary>
+        /// Generages the token.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="ExpiresDelayInMinutes">The expires delay in minutes.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>JWT</returns>
+        string? GenerageToken(User user, DateTime issuedAtUtc, TimeSpan ExpiresDelayInMinutes, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Validates jwt.
+        /// </summary>
+        /// <param name="token">JWT.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>User name from token.</returns>
+        Guid? ValidateToken(string token, CancellationToken cancellationToken);
+    }
+}
