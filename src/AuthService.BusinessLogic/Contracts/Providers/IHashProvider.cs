77615f0eb5436b0a8f634635fcb729f15ab6505b
bynikk <bynik2001@gmail.com>
2023-02-20 10:58:57 +0300

    Implement hash provider

---
+++ src/AuthService.BusinessLogic/Contracts/Providers/IHashProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace AuthService.BusinessLogic.Contracts.Providers
+{
+    /// <summary>
+    /// Hash provider contract.
+    /// </summary>
+    public interface IHashProvider
+    {
+        /// <summary>
+        /// Hashes the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Hashed string.</returns>
+        string Hash(string value);
+
+        /// <summary>
+        /// Verifies the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="hash">The hash.</param>
+        /// <returns>True if vefification sucess and false if vefification failed.</returns>
+        bool Verify(string value, string hash);
+    }
+}
