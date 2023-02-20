f0479eb37d8b61a3f940381df72258de10eb495a
bynikk <bynik2001@gmail.com>
2023-02-20 12:09:27 +0300

    Register DI for infrastructure services

---
+++ src/AuthService.Grpc/Infrastructure/Configurations/AppConfiguration.Services.cs
@@ -0,0 +1,46 @@
+﻿using AuthService.BusinessLogic;
+using AuthService.BusinessLogic.Contracts.Generators;
+using AuthService.BusinessLogic.Contracts.Providers;
+using AuthService.BusinessLogic.Contracts.Services;
+using AuthService.BusinessLogic.Contracts.Worker;
+using AuthService.BusinessLogic.Generators;
+using AuthService.BusinessLogic.Providers;
+using AuthService.BusinessLogic.Services;
+using AuthService.BusinessLogic.Workers;
+using BusinessLogic = AuthService.BusinessLogic;
+
+namespace AuthService.Grpc.Infrastructure.Configurations
+{
+    /// <summary>
+    /// App configuration
+    /// </summary>
+    public static partial class AppConfiguration
+    {
+        /// <summary>Adds the infrastructure services to service collection</summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns> IServiceCollection </returns>
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Program).Assembly);
+
+            return services;
+        }
+
+        /// <summary>Adds the infrastructure services to service collection</summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns> IServiceCollection  </returns>
+        public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, BusinessLogic.Services.AuthService>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<ITokenGenerator, TokenGenerator>()
+                .AddScoped<IJwtTokenGenerator, JWTTokenGenerator>()
+                .AddScoped<IRefreshTokenGenerator, RefreshTokenGenerator>()
+                .AddScoped<IHashProvider, HashProvider>()
+                .AddScoped<IJWTWorker, JWTWorker>()
+                .AddScoped<IDateTimeProvider, DateTimeProvider>();
+
+            return services;
+        }
+    }
+}
